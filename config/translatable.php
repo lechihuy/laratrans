c728b0b113586219b28f028d6298e92c12e41ece
Lê Chí Huy <lechihuy1062001@gmail.com>
2021-10-09 21:05:44 +0700

    chore: update configuration

--- config/translatable.php
+++ config/translatable.php
@@ -1,10 +1,27 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Locales
+    |--------------------------------------------------------------------------
+    |
+    | The locales are allowed to interact with your application.
+    |
+    */
     'locales' => [
         'vi',
         'en'
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | The name of locale column's traslation tables
+    |--------------------------------------------------------------------------
+    |
+    | It must be matched with the name of locale column's translation tables in your application.
+    | You can also configure it in each translation model.
+    |
+    */
     'locale_key' => 'locale',
 ];
